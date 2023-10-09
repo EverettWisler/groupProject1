daac899c315aa3ecb1058f057cd48469b0fb8cd8
WISLERER22 <Wislerer22@gcc.edu>
2023-10-09 13:30:52 -0400

    i made changes

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
-
+import java.util.ArrayList;
 public class Main {
+    private ArrayList<String> namesArrayList;
+    public static void loadDataBase(){
+
+    }
     public static void main(String[] args) {
         System.out.print("Enter your name: ");
         Scanner input = new Scanner(System.in);
